522b7f3bebf829ad93100643167319c6d63ba860
kimyoungjin12 <qazyj@naver.com>
2019-11-21 20:37:30 +0900

    commit

---
+++ BABEREC.Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using BABEREC.Client.Helpers;
+
+namespace BABEREC.Client.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ BABEREC.Client/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using BABEREC.Client.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace BABEREC.Client.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
